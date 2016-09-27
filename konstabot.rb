fab6e97dd8affda5524965980bff810da85a64e7
Konstantin Tchernov <konstantin.tchernov@pushpay.com>
2016-09-27 21:52:57 +1300

    Make more parameters configurable including RT/like count and name. Plus a "no-history" mode to help debugging.

--- konstabot.rb
+++ konstabot.rb
@@ -16,6 +16,10 @@ option_parser = OptionParser.new do |opts|
 	opts.on('-d', '--dry-run', 'Log output without posting to Slack') do
 		options[:dry_run] = true
 	end
+
+	opts.on('-H', '--no-history', 'Ignore the history of the last emitted tweet, scan through all tweets') do
+		options[:no_history] = true
+	end
 end
 
 begin
@@ -44,72 +48,82 @@ consumer = OAuth::Consumer.new(
 
 access_token = OAuth::AccessToken.new(consumer, config['token'], config['secret'])
 
+history=nil
 begin
-	history=YAML.load_file('history.yml')
+	history=YAML.load_file('history.yml') unless options[:no_history]
 rescue
 # ignored
 end
 
-blacklist_set = config['blacklisted_words'].map { |word| word.downcase }
-
 path_to_query="/#{api_version}/lists/statuses.json?owner_screen_name=#{config['twitter_user']}&slug=#{config['twitter_list']}"
 path_to_query+="&since_id=#{history[:latest_emitted_id]}" unless history.nil?
-path_to_query+='&include_rts=0&count=100&include_entities=false'
+path_to_query+="&include_rts=0&count=#{config['num_tweets_to_fetch']}&include_entities=false"
 response=access_token.get(path_to_query)
 
 exit 1 unless response.class == Net::HTTPOK
 
 tweets=JSON.parse(response.body)
 
-tweets.select! { |tweet| tweet['retweet_count'] >= 2 || tweet['favorite_count'] >= 3 }
+tweets.select! { |tweet|
+	tweet['retweet_count'] >= config['retweet_min_count'] || tweet['favorite_count'] >= config['likes_min_count'] }
 
-tweets.reject! { |tweet| tweet['text'].split(/[^\w]+/).any? { |k| blacklist_set.member?(k.downcase) } }
+# Split out words, compare them to blacklist.
+blacklist_set = config['blacklisted_words'].map { |word| word.downcase }
+tweets.reject! { |tweet| tweet['text'].split(/[^\w]+/).any? { |word| blacklist_set.member?(word.downcase) } }
 
-tweets.sort! { |b, a| a['retweet_count'] + a['favorite_count'] <=> b['retweet_count'] + b['favorite_count'] }
-
-# Same account can only be emitted 2 times
-seen_users = {}
-tweets_to_emit = []
-tweets.each { |tweet|
-	screen_name = tweet['user']['screen_name']
-
-	seen_users[screen_name] = 0 unless seen_users.key? screen_name
-	next if seen_users[screen_name] >= 1
-
-	seen_users[screen_name]=seen_users[screen_name]+1
-
-	tweets_to_emit << tweet
-
-	break if tweets_to_emit.length == options[:number_of_tweets]
+tweets.sort! { |tweetA, tweetB|
+	tweetB['retweet_count'] + tweetB['favorite_count'] <=> tweetA['retweet_count'] + tweetB['favorite_count']
 }
+
+# Same account can only be emitted emit_max_tweets_per_user times per session
+if config.key? 'emit_max_tweets_per_user'
+	seen_users = {}
+	tweets_to_emit = []
+	tweets.each { |tweet|
+		screen_name = tweet['user']['screen_name']
+
+		seen_users[screen_name] = 0 unless seen_users.key? screen_name
+		next if seen_users[screen_name] >= config['emit_max_tweets_per_user']
+
+		seen_users[screen_name]=seen_users[screen_name]+1
+
+		tweets_to_emit << tweet
+
+		break if tweets_to_emit.length == options[:number_of_tweets]
+	}
+else
+	tweets_to_emit = tweets.take options[:number_of_tweets]
+end
 
 latest_emitted_id=0
 
-for item in tweets_to_emit
-	msg_text = item['text']
-	user_name = item['user']['screen_name']
-	id=item['id']
-	id_str = item['id_str']
-	icon_url = item['user']['profile_image_url_https']
+# Send the Tweets to Slack
+tweets_to_emit.each { |tweet|
+	msg_text = tweet['text']
+	user_name = tweet['user']['screen_name']
+	id=tweet['id']
+	id_str = tweet['id_str']
+	icon_url = tweet['user']['profile_image_url_https']
 
 	latest_emitted_id=id if id > latest_emitted_id
 
-	unless options[:dry_run]
+	twitter_status_uri = "https://twitter.com/#{user_name}/statuses/#{id_str}"
+	if options[:dry_run]
+		puts "[#{twitter_status_uri}] #{user_name}: #{msg_text}"
+	else
 		HTTParty.post config['slack_webhooks_uri'], {
 				:body => {
-						:text => "https://twitter.com/#{user_name}/statuses/#{id_str}",
-						:username => "#{user_name} (Konstabot)",
+						:text => twitter_status_uri,
+						:username => "#{user_name} (#{config['bot_name']})",
 						:icon_url => icon_url
 				}.to_json,
 				:headers => {'Content-Type' => 'application/json'}
 		}
-	else
-		puts "#{user_name}: #{msg_text}"
 	end
-end
+}
 
-unless latest_emitted_id==0
-	history={:latest_emitted_id => latest_emitted_id}
+unless options[:dry_run] || options[:no_history] || latest_emitted_id == nil
+	updated_history={:latest_emitted_id => latest_emitted_id}
 
-	File.open('history.yml', 'w') { |f| f.write history.to_yaml }
+	File.open('history.yml', 'w') { |f| f.write updated_history.to_yaml }
 end
