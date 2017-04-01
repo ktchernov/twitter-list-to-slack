7f3d3b0792f1c94902b602c52d61327ecc43e9ee
Konstantin Tchernov <konstantin.tchernov@xero.com>
2017-04-01 16:23:48 +1300

    Support configuring credentials from environment variables

--- konstabot.rb
+++ konstabot.rb
@@ -36,11 +36,32 @@ rescue OptionParser::ParseError
 end
 
 credentials=nil
+
+def credentials_from_env
+	if ENV['KB_CONSUMER_KEY'] == nil
+		return nil
+	end
+
+	credentials = {
+		consumer_key: ENV['KB_CONSUMER_KEY'],
+		consumer_secret: ENV['KB_CONSUMER_SECRET'],
+		token: ENV['KB_TOKEN'],
+		secret: ENV['KB_SECRET'],
+		slack_webhooks_uri: ENV['KB_SLACK_WEBHOOKS_URI']
+	}
+	return credentials
+end
+
 begin
 	credentials=YAML.load_file('credentials.yml')
 rescue
-	print 'Please create a credentials.yml following the example in README.md'
-	exit 1
+	credentials = credentials_from_env
+
+	if credentials == nil
+		print 'Please create a credentials.yml or define environment variables '
+		  + 'following the examples in README.md'
+		exit 1
+	end
 end
 
 api_version = '1.1'
